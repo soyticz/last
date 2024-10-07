59cfa359b0f47d27bac6c4c6cab6df1bf06070df
soyticz12 <jadoc251113@cubao.sti.edu.ph>
2024-10-07 20:43:31 +0800

    asd

--- Views/EmployeesView/AddWindowView/AddWindowsView.xaml.cs
+++ Views/EmployeesView/AddWindowView/AddWindowsView.xaml.cs
@@ -1,4 +1,7 @@
 using System.Windows;
+using System.Windows.Input;
+using System.Windows.Controls; // Corrected this line
+using System.Windows.Media;
 using wpf1.ViewModels;
 using wpf1.Models;
 using Google.Cloud.Firestore.V1;
@@ -7,24 +10,27 @@ namespace wpf1.Views.EmployeesView.AddWindowView
 {
     public partial class AddWindowsView : Window
     {
-
         public AddWindowsView()
         {
             InitializeComponent();
         }
 
+        // Change button background color on mouse enter
         private void Button_MouseEnter(object sender, MouseEventArgs e)
-{
-    var button = sender as Button;
-    button.Background = new SolidColorBrush(Color.FromRgb(102, 51, 153)); // Darker color on hover
-}
+        {
+            if (sender is Button button) // Use pattern matching
+            {
+                button.Background = new SolidColorBrush(Color.FromRgb(102, 51, 153)); // Darker color on hover
+            }
+        }
 
-private void Button_MouseLeave(object sender, MouseEventArgs e)
-{
-    var button = sender as Button;
-    button.Background = new SolidColorBrush(Color.FromRgb(120, 79, 242)); // Original color
-}
-
-        
+        // Reset button background color on mouse leave
+        private void Button_MouseLeave(object sender, MouseEventArgs e)
+        {
+            if (sender is Button button) // Use pattern matching
+            {
+                button.Background = new SolidColorBrush(Color.FromRgb(120, 79, 242)); // Original color
+            }
+        }
     }
 }
