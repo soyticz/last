6f25672e5ee943cd8d20975d6aa5bb493aa5f752
soyticz <143929580+soyticz@users.noreply.github.com>
2024-10-07 10:21:40 +0000

    Auto-commit on 2024-10-07 10:21:40

---
+++ Commands/RelayCommand.cs
@@ -0,0 +1,25 @@
+using System;
+using wpf1.Abstracts;
+namespace wpf1.Commands
+{
+    public class RelayCommand<T> : CommandBase
+    {
+        private readonly Action<T> _execute;
+        private readonly Predicate<T>? _canExecute;
+
+        public RelayCommand(Action<T> execute, Predicate<T>? canExecute = null)
+        {
+            _execute = execute ?? throw new ArgumentNullException(nameof(execute));
+            _canExecute = canExecute;
+        }
+
+        public override bool CanExecute(object? parameter) =>
+            _canExecute == null || (parameter is T t && _canExecute(t));
+
+        public override void Execute(object? parameter)
+        {
+            if (parameter is T t)
+                _execute(t);
+        }
+    }
+}
