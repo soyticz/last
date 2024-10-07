023747110658fa07bc51b9a63e0c2748c7c55c02
soyticz <143929580+soyticz@users.noreply.github.com>
2024-10-07 10:54:09 +0000

    Auto-commit on 2024-10-07 10:54:09

--- Views/EmployeesView/EmployeesDataGrid/EmployeesDataGrid.xaml.cs
+++ Views/EmployeesView/EmployeesDataGrid/EmployeesDataGrid.xaml.cs
@@ -13,20 +13,8 @@ namespace wpf1.Views.EmployeesView.EmployeesDataGrid
         }
         private void Edit_Button(object sender, RoutedEventArgs e)
         {
-            // Get the EID from the CommandParameter
-            var button = sender as Button;
-            if (button != null)
-            {
-                string eid = button.CommandParameter as string;
-
-                // Now you can use the EID to load the corresponding employee data for editing
-                if (!string.IsNullOrEmpty(eid))
-                {
-                    // Open the Edit window and pass the EID
-                    var _editWindowView = new EditWindowView.EditWindowView(eid);
-                    _editWindowView.ShowDialog();
-                }
-            }
+           
+          
         }
     }
 }
