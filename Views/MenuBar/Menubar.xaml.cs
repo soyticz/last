a050ddfe6cdcd4a45868d83cd10fb944410ab6f1
soyticz <143929580+soyticz@users.noreply.github.com>
2024-10-07 09:51:53 +0000

    Auto-commit on 2024-10-07 09:51:53

---
+++ Views/MenuBar/Menubar.xaml.cs
@@ -0,0 +1,15 @@
+using System.Windows.Controls;
+using System.Windows;
+
+namespace wpf1.Views.MenuBar;
+
+public partial class Menubar : UserControl
+
+{
+    public Menubar()
+    {
+        InitializeComponent();
+    }
+
+    
+}
