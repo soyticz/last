168cb0193bee8e7ffbf0cd72f33c574bfcd76e19
soyticz <143929580+soyticz@users.noreply.github.com>
2024-10-07 09:12:41 +0000

    Auto-commit on 2024-10-07 09:12:41

--- wpf1/MainWindow.xaml.cs
+++ wpf1/MainWindow.xaml.cs
@@ -7,6 +7,8 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
+using System.Collections.Generic;
+using System;
 using System.Windows.Shapes;
 
 namespace wpf1;
@@ -16,8 +18,40 @@ namespace wpf1;
 /// </summary>
 public partial class MainWindow : Window
 {
+    
     public MainWindow()
     {
         InitializeComponent();
+       
     }
+    private void Border_MouseDown(object sender, MouseButtonEventArgs e)
+    {
+        if (e.ChangedButton == MouseButton.Left)
+        {
+            this.DragMove();
+        }
+    }
+
+    private bool isMaximized = false;
+
+    private void Border_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+    {
+        if (e.ClickCount == 2)
+        {
+            if (isMaximized)
+            {
+                this.WindowState = WindowState.Normal;
+                this.Width = 1080;
+                this.Height = 720;
+
+                isMaximized = false;
+            }
+            else
+            {
+                this.WindowState = WindowState.Normal;
+                isMaximized = true;
+            }
+        }
+    }
+
 }
