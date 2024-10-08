80c52930e93c11d94489b769d5f641b31b86c10a
soyticz <143929580+soyticz@users.noreply.github.com>
2024-10-08 16:39:43 +0000

    Auto-commit on 2024-10-08 16:39:43

--- Views/PatientView/DesignPatients/DesignPatientsView.xaml.cs
+++ Views/PatientView/DesignPatients/DesignPatientsView.xaml.cs
@@ -2,7 +2,7 @@ using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
 
-namespace wpf1.Views.PatientView
+namespace wpf1.Views.PatientView.DesignPatients
 {
     public partial class DesignPatientsView : UserControl
     {
