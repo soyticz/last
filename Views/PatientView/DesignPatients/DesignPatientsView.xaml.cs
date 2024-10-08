679ff3454df44774ebf8dcfe2e43347eca211fe5
soyticz <143929580+soyticz@users.noreply.github.com>
2024-10-08 17:18:05 +0000

    Auto-commit on 2024-10-08 17:18:05

--- Views/PatientView/DesignPatients/DesignPatientsView.xaml.cs
+++ Views/PatientView/DesignPatients/DesignPatientsView.xaml.cs
@@ -10,32 +10,5 @@ namespace wpf1.Views.PatientView.DesignPatients
         {
             InitializeComponent();
         }
-
-        private void PatientCard_MouseDown(object sender, MouseButtonEventArgs e)
-        {
-            PatientPopup.IsOpen = true;
-            if (sender is Border patientCard && PatientPopup.IsOpen == true)
-            {
-                // Get the name of the patient from the TextBlock in the card
-                var textBlock = (TextBlock)((Grid)patientCard.Child).Children[1];
-                string patientName = textBlock.Text;
-
-                // Example of patient details, you can customize this with actual data
-                string patientDetails = "Patient Age: 30\nCondition: Healthy\nLast Visit: 01/01/2024";
-
-                // Set the Popup content
-                PopupPatientName.Text = patientName;
-                PopupPatientDetails.Text = patientDetails;
-
-                // Show the Popup
-                
-            }
-        }
-
-        private void ClosePopupButton_Click(object sender, RoutedEventArgs e)
-        {
-            // Close the Popup
-            PatientPopup.IsOpen = false;
-        }
     }
 }
