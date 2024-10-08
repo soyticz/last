3e942b698b884e2839afa49acc0f76bd718ff14a
soyticz <143929580+soyticz@users.noreply.github.com>
2024-10-08 05:33:33 +0000

    Auto-commit on 2024-10-08 05:33:33

--- Views/ScheduleView/ScheduleDatagridView/ScheduleDatagridView.xaml.cs
+++ Views/ScheduleView/ScheduleDatagridView/ScheduleDatagridView.xaml.cs
@@ -1,4 +1,5 @@
 using System.Windows.Controls;
+using wpf1.ViewModels;
 
 namespace wpf1.Views.ScheduleView.ScheduleDatagridView;
 public partial class ScheduleDatagridView : UserControl
@@ -6,5 +7,6 @@ public partial class ScheduleDatagridView : UserControl
     public ScheduleDatagridView()
     {
         InitializeComponent();
+        this.DataContext = new ScheduleDatagridViewModel();
     }
 }
