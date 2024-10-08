a85a7a01124108b157a30c4fb87b2e0a9eec4463
soyticz <143929580+soyticz@users.noreply.github.com>
2024-10-08 16:44:43 +0000

    Auto-commit on 2024-10-08 16:44:43

--- Views/PatientView/DesignPatients/DesignPatientsView.xaml.cs
+++ Views/PatientView/DesignPatients/DesignPatientsView.xaml.cs
@@ -1,6 +1,5 @@
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Input;
 
 namespace wpf1.Views.PatientView.DesignPatients
 {
@@ -11,15 +10,17 @@ namespace wpf1.Views.PatientView.DesignPatients
             InitializeComponent();
         }
 
-        private void PatientCard_MouseDown(object sender, MouseButtonEventArgs e)
+        private void PatientCard_MouseDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
         {
-            if (sender is Border card)
+            if (sender is Border patientCard)
             {
-                // Get the patient name from the Tag property
-                string patientName = card.Tag.ToString();
-                
-                // Update the bottom details display
-                PatientDetailsTextBlock.Text = $"Details for {patientName}.";
+                // Get the name of the patient from the TextBlock in the card
+                var textBlock = (TextBlock)((Grid)patientCard.Child).Children[1];
+                string patientName = textBlock.Text;
+
+                // Update the popup text and show it
+                PopupTextBlock.Text = $"Details for {patientName}"; // Customize this with actual details
+                PatientDetailsPopup.IsOpen = true;
             }
         }
     }
