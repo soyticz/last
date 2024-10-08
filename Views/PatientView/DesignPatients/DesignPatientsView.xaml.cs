378358c813c886d4bc993e3a909b82fced1f8cf2
soyticz <143929580+soyticz@users.noreply.github.com>
2024-10-08 16:36:54 +0000

    Auto-commit on 2024-10-08 16:36:54

--- Views/PatientView/DesignPatients/DesignPatientsView.xaml.cs
+++ Views/PatientView/DesignPatients/DesignPatientsView.xaml.cs
@@ -1,18 +1,26 @@
-using System.Collections.Generic;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Input;
 
-namespace wpf1.Views.PatientView.DesignPatients
+namespace wpf1.Views.PatientView
 {
     public partial class DesignPatientsView : UserControl
     {
-        // Sample patient data model
-       
-          
-
         public DesignPatientsView()
         {
             InitializeComponent();
-        }  
+        }
+
+        private void PatientCard_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (sender is Border card)
+            {
+                // Get the patient name from the Tag property
+                string patientName = card.Tag.ToString();
+                
+                // Update the bottom details display
+                PatientDetailsTextBlock.Text = $"Details for {patientName}.";
+            }
+        }
     }
 }
