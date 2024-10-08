b19a18255ba570a47c3c158280e70cc2a75c093b
soyticz <143929580+soyticz@users.noreply.github.com>
2024-10-08 12:51:50 +0000

    Auto-commit on 2024-10-08 12:51:50

--- Views/PatientView/PatientDataGridView/PatientDatagridView.xaml.cs
+++ Views/PatientView/PatientDataGridView/PatientDatagridView.xaml.cs
@@ -1,4 +1,5 @@
 using System.Windows.Controls;
+using wpf1.ViewModels;
 
 namespace wpf1.Views.PatientView.PatientDataGridView;
 public partial class PatientDatagridView : UserControl
@@ -6,5 +7,6 @@ public partial class PatientDatagridView : UserControl
     public PatientDatagridView()
     {
         InitializeComponent();
+        this.DataContext = PatientDatagridViewModel();
     }
 }
